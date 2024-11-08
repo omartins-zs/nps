aca8b45d881bae4ed6a09ee6fc061b782c79f0d0
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-08 08:18:18 -0300

    Criando o Template Footer

---
+++ resources/views/templates/footer.blade.php
@@ -0,0 +1,9 @@
+<!-- resources/views/templates/footer.blade.php -->
+<footer class="">
+
+</footer>
+
+
+<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gray-700 text-white text-center py-2 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+    <p>&copy; {{ date('Y') }} Sistema NPS. Todos os direitos reservados.</p>
+</footer>
