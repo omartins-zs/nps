5e619ef7081432894655836a09baf46b0745880a
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-08 15:19:49 -0300

    Add Rota de Login,Nps e Test Connection

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\NPSController;
+use App\Http\Controllers\TestConnectionController;
 
 // Route::get('/', function () {
 //     return view('welcome');
@@ -9,3 +11,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 })->name('home');
+
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
+Route::get('/nps', [NPSController::class, 'index']);
+Route::get('/test-connection', [TestConnectionController::class, 'testConnectionMysql']);
