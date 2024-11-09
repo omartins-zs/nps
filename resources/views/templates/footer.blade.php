a677977009155306010e342e4c3c69053149cece
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-09 13:56:49 -0300

    Ajuste no Footer

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,9 +1,4 @@
 <!-- resources/views/templates/footer.blade.php -->
-<footer class="">
-
-</footer>
-
-
-<footer class="fixed bottom-0 left-0 z-20 w-full p-4 bg-gray-700 text-white text-center py-2 border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+<footer class="fixed bottom-0 left-0 z-20 w-full p-2 bg-gray-700 text-white text-center py-3 border-t border-gray-200 shadow flex justify-center items-center dark:bg-gray-800 dark:border-gray-600">
     <p>&copy; {{ date('Y') }} Sistema NPS. Todos os direitos reservados.</p>
 </footer>
