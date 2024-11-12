404f1a3194fbceefa8bef8f6605e754992d4d93a
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-12 16:24:07 -0300

    Comentando Links inutilizados no Sidebar

--- resources/views/templates/sidebar.blade.php
+++ resources/views/templates/sidebar.blade.php
@@ -22,7 +22,7 @@
                     <span class="ms-3">Dashboard</span>
                 </a>
             </li>
-            <li>
+            {{-- <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -69,7 +69,7 @@
                     </svg>
                     <span class="flex-1 ms-3 whitespace-nowrap">Products</span>
                 </a>
-            </li>
+            </li> --}}
             <li>
                 <a href="#"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
