b496ab0c33e5e6c6cc0ca7f0e48a7e68aa9114bd
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-18 08:27:14 -0300

    Add From no Navbar e Ajuste do Route

--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -116,7 +116,8 @@
 
 <!-- Segunda linha com selects e botão de pesquisa -->
 <div class="bg-white py-1 border-t border-gray-200">
-    <div class="max-w-full mx-auto px-4 sm:px-6 lg:px-8 flex justify-end items-center space-x-3">
+    <form action="{{ route('dashboard') }}" method="GET"
+        class="max-w-full mx-auto px-4 sm:px-6 lg:px-8 flex justify-end items-center space-x-3">
         <!-- Select de Mês -->
         <div>
             <select id="selectedMonth" name="selectedMonth"
@@ -136,10 +137,10 @@
         </div>
 
         <!-- Botão de Pesquisa -->
-        <button type="button"
+        <button type="submit"
             class="ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center">
             <i class="fas fa-search mr-2"></i>
             Consultar
         </button>
-    </div>
+    </form>
 </div>
