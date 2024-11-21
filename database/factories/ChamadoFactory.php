79366439e352fc132e8de503553c7e76d26be383
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 23:40:40 -0300

    Ajustando a Factory de Chamados

--- database/factories/ChamadoFactory.php
+++ database/factories/ChamadoFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,16 @@ class ChamadoFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'titulo' => $this->faker->sentence,
+            'descricao' => $this->faker->paragraph,
+            'usuario_criador_id' => User::inRandomOrder()->first()->id, // Associando a um usuário aleatório
+            'usuario_responsavel_id' => User::inRandomOrder()->first()->id, // Associando a um usuário aleatório
+            'status' => $this->faker->randomElement(['aberto', 'em_andamento', 'concluido']),
+            'data_previsao' => $this->faker->date(),
+            'data_finalizacao' => $this->faker->date(),
         ];
     }
 }
