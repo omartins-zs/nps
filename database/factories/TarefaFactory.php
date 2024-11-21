421347636e017551968d205f572b599cf0f5d89a
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 23:49:10 -0300

    Ajustando a Tarefa Factory

--- database/factories/TarefaFactory.php
+++ database/factories/TarefaFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Chamado;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -14,10 +15,17 @@ class TarefaFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition(): array
+    public function definition()
     {
         return [
-            //
+            'chamado_id' => Chamado::inRandomOrder()->first()->id, // Associando a um chamado aleatório
+            'tipo_tarefa' => $this->faker->randomElement(['atendimento', 'melhoria', 'novo projeto', 'manutencao']),
+            'horas_gastas' => $this->faker->randomFloat(2, 0, 8), // Horas gastas aleatórias (até 8 horas)
+            'horas_previstas' => $this->faker->randomFloat(2, 0, 8), // Horas previstas aleatórias (até 8 horas)
+            'status' => $this->faker->randomElement(['em andamento', 'concluida']),
+            'descricao' => $this->faker->sentence,
+            'data_previsao' => $this->faker->date(),
+            'data_finalizacao' => $this->faker->date(),
         ];
     }
 }
