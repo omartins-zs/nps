714a1eb57dbc9825a9fa318c027c59c39164f1b4
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 22:02:03 -0300

    Ajustando Estrutura da Tabela de Chamados

--- database/migrations/2024_11_21_005720_create_chamados_table.php
+++ database/migrations/2024_11_21_005720_create_chamados_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('chamados', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->unsignedBigInteger('usuario_criador_id');
+            $table->unsignedBigInteger('usuario_responsavel_id')->nullable();
+            $table->enum('status', ['aberto', 'em_andamento', 'concluido'])->default('aberto');
+            $table->date('data_previsao')->nullable();
+            $table->date('data_finalizacao')->nullable();
             $table->timestamps();
+
+            $table->foreign('usuario_criador_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('usuario_responsavel_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
