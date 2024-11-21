bd939bdb839ec3bd7447d165822ae97bc7150d37
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 22:17:52 -0300

    Ajustando a Factory de User

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,14 +4,13 @@ namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
  */
 class UserFactory extends Factory
 {
-    /**
+   /**
      * The current password being used by the factory.
      */
     protected static ?string $password;
@@ -28,17 +27,6 @@ class UserFactory extends Factory
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
