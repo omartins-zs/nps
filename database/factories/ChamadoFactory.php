7c56a0197084e042616bcf86400b773d16e4cc4f
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 23:52:30 -0300

    Ajustando o Extends das Factorys com os Models

--- database/factories/ChamadoFactory.php
+++ database/factories/ChamadoFactory.php
@@ -6,7 +6,7 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Chamado>
  */
 class ChamadoFactory extends Factory
 {
