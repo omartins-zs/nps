c7df49dc9324fc3bb6d53b561a90dfcb4c547e89
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 23:45:39 -0300

    Ajustando o Model de Tarefas do Chamado

--- app/Models/Tarefa.php
+++ app/Models/Tarefa.php
@@ -2,9 +2,37 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Tarefa extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'chamado_id',
+        'tipo_tarefa',
+        'horas_gastas',
+        'horas_previstas',
+        'status',
+        'descricao',
+        'data_previsao',
+        'data_finalizacao',
+    ];
+
+    /**
+     * Relacionamento com o chamado
+     */
+    public function chamado()
+    {
+        return $this->belongsTo(Chamado::class, 'chamado_id');
+    }
+
+    /**
+     * Relacionamento com o usuário (responsável pela tarefa)
+     */
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
 }
