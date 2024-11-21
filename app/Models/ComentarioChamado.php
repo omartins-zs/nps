6678149d4c6bdd31f69a78bc8b2382addb119ece
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-20 23:48:27 -0300

    Ajustando o Model de Comentarios do Chamado

--- app/Models/ComentarioChamado.php
+++ app/Models/ComentarioChamado.php
@@ -2,9 +2,32 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ComentarioChamado extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'ticket_id',
+        'user_id',
+        'comment',
+    ];
+
+    /**
+     * Relacionamento com o chamado
+     */
+    public function chamado()
+    {
+        return $this->belongsTo(Chamado::class, 'ticket_id');
+    }
+
+    /**
+     * Relacionamento com o usuário (autor do comentário)
+     */
+    public function usuario()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
