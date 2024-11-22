30d0daa4c65d055168a5b6754fab56d635e8a842
omartins-zs <gabrielmatheus209@gmail.com>
2024-11-21 21:11:27 -0300

    Ajustando a Seeder de Notas para Popular o BD

--- database/seeders/NotaSeeder.php
+++ database/seeders/NotaSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class NotaSeeder extends Seeder
 {
@@ -12,6 +14,14 @@ class NotaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        foreach (range(1, 50) as $index) {
+            DB::table('notas')->insert([
+                'usuario_id' => rand(1, 10), // ID aleatório de usuário
+                'nota' => rand(0, 10), // Nota aleatória entre 0 e 10
+                'data_resposta' => Carbon::now()->subDays(rand(0, 30))->format('Y-m-d'), // Data aleatória nos últimos 30 dias
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
