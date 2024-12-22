39fdf59b30dc91ddef7331ddd6557b0574387dd4
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-22 12:33:15 -0300

    Fazendo Loop dos Seeders pelo 12 Meses do Ano

--- database/seeders/NotaSeeder.php
+++ database/seeders/NotaSeeder.php
@@ -14,14 +14,19 @@ class NotaSeeder extends Seeder
      */
     public function run(): void
     {
-        foreach (range(1, 50) as $index) {
-            DB::table('notas')->insert([
-                'usuario_id' => rand(1, 10), // ID aleatório de usuário
-                'nota' => rand(0, 10), // Nota aleatória entre 0 e 10
-                'data_resposta' => Carbon::now()->subDays(rand(0, 30))->format('Y-m-d'), // Data aleatória nos últimos 30 dias
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+        $year = date('Y'); // Ano atual
+
+        // Loop pelos 12 meses do ano
+        foreach (range(1, 12) as $month) {
+            foreach (range(1, 50) as $index) {
+                DB::table('notas')->insert([
+                    'usuario_id' => rand(1, 10), // ID aleatório de usuário
+                    'nota' => rand(0, 10), // Nota aleatória entre 0 e 10
+                    'data_resposta' => Carbon::create($year, $month, rand(1, 28))->format('Y-m-d'), // Data aleatória no mês
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ]);
+            }
         }
     }
 }
