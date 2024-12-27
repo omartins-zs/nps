4c1ccbc4646cf39b8ab71ff16a11b7ee8cefb41c
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-27 01:36:54 -0300

    Ajustando Migration de Departamentos e Pesquisas

--- database/migrations/2024_12_27_043327_criar_tabela_departamentos.php
+++ database/migrations/2024_12_27_043327_criar_tabela_departamentos.php
@@ -11,7 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome')->unique();
+            $table->timestamps('criado_em', 'atualizado_em');
+        });
     }
 
     /**
@@ -19,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('departamentos');
     }
 };
--- database/migrations/2024_12_27_043545_create_pesquisas_table.php
+++ database/migrations/2024_12_27_043545_create_pesquisas_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('pesquisas', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('titulo');
+            $table->text('descricao')->nullable();
+            $table->foreignId('departamento_id')->constrained('departamentos')->onDelete('cascade');
+            $table->date('prazo');
+            $table->timestamps('criado_em', 'atualizado_em');
         });
     }
 
