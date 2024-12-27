1b177b4c761e25dcff147a8c7880b9e8e753b37d
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-27 01:43:02 -0300

    Criando Rotas para respostas e dashboard

--- routes/web.php
+++ routes/web.php
@@ -9,4 +9,6 @@ Route::get('/', function () {
 Route::middleware(['auth'])->group(function () {
     Route::resource('departamentos', DepartamentoController::class);
     Route::resource('pesquisas', PesquisaController::class);
+    Route::resource('respostas', RespostaController::class);
+    Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
 });
