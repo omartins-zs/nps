3b4b40689c7a22a194fc36a70d27198de43d08f1
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-28 13:16:07 -0300

    Ajustando Model do Response

--- app/Models/Response.php
+++ app/Models/Response.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Response extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['survey_id', 'score', 'comment'];
+
+    public function survey()
+    {
+        return $this->belongsTo(Survey::class);
+    }
 }
