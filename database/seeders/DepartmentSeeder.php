9b3d21b2f28f3d894be9d51affbd4d1eb3515816
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-28 13:21:33 -0300

    Create Seeder de Departamentos

--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -4,14 +4,21 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Department;
+use App\Models\Survey;
 
 class DepartmentSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        //
+        Department::create(['name' => 'RH']);
+        Department::create(['name' => 'TI']);
+        Department::create(['name' => 'Compras']);
+        Department::create(['name' => 'Almoxarifado']);
+        Department::create(['name' => 'Diretoria']);
+        Department::create(['name' => 'Marketing']);
     }
 }
