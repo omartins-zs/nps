61fed01312db63e2fc96ce1a04acbc8e70395148
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-28 13:16:16 -0300

    Ajustando Model do Survey

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -2,9 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['title', 'department_id'];
+
+    public function department()
+    {
+        return $this->belongsTo(Department::class);
+    }
+
+    public function responses()
+    {
+        return $this->hasMany(Response::class);
+    }
 }
