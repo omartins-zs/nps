3abcd87185bce1b3ba2fa79b3b25a68d0ed7288c
omartins-zs <gabrielmatheus209@gmail.com>
2024-12-29 20:42:15 -0300

    Add Flowbite e Dark Mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,14 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 
 /** @type {import('tailwindcss').Config} */
 export default {
+    darkMode:  'class', // Habilita o suporte ao tema escuro com a classe 'dark'
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
         './storage/framework/views/*.php',
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './node_modules/flowbite/**/*.js'  // Adicione essa linha
     ],
     theme: {
         extend: {
@@ -16,5 +18,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [
+        require('flowbite/plugin')
+    ],
 };
